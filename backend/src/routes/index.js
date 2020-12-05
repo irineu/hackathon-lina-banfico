8d89821c06a83f0e182ab9c4d6ab3fc09927532d
irineu <irineu@irineuantunes.com>
2020-12-05 20:24:01 -0300

    ajustes path

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ import accountUnblock from './services/accountUnblockService.js';
 import accountPassword from './services/accountPasswordService.js';
 import echo from './services/echo.js';
 import user from './services/userService.js';
+import fs from 'fs';
 
 import httpUtils from '../util/http.js';
 
@@ -22,6 +23,40 @@ export default {
         global.httpServer.use('/services/account-password', accountPassword);
         global.httpServer.use('/services/user', user);
 
-        global.httpServer.use('/services/echo', echo);        
+        global.httpServer.use('/services/echo', echo);  
+        
+        global.httpServer.use('/members-area/*', (req, res) => {
+
+            let d = path.resolve('../frontend/dist/hackathon-lina-banfico');
+            let directory = req.originalUrl.substr(req.originalUrl.indexOf("members-area") + 12);
+            console.log(d + directory, fs.existsSync(d +directory));
+            try {
+                if (fs.existsSync(d + directory)) {
+                    console.log(d + directory, fs.existsSync(d +directory));
+                    res.sendFile(`${d}${directory}`);
+                }else{
+                    res.sendFile(`${d}/index.html`);
+                }
+            } catch(err) {
+                res.sendFile(`${d}/index.html`);
+            }
+             
+        });
+        
+        global.httpServer.use('/*', (req, res) => {
+
+            let d = path.resolve('../landing/dist/landing');
+            try {
+                if (fs.existsSync(d + req.originalUrl)) {
+                    
+                    res.sendFile(`${d}${req.originalUrl}`);
+                }else{
+                    res.sendFile(`${d}/index.html`);
+                }
+            } catch(err) {
+                res.sendFile(`${d}/index.html`);
+            }
+             
+        });
     }
 };
